280c852e244f9606193548131dfeed3ebf6e6bbb
PurinLord <purin@tutanota.de>
2015-10-21 15:37:43 -0500

    mor flexible working fakeSub

--- biojava/Substitutor.java
+++ biojava/Substitutor.java
@@ -11,24 +11,41 @@ import org.biojava.bio.structure.AminoAcid;
 
 public class Substitutor{
 
-Structure fitStruc = null;
+Structure modifyStructure = null;
 PDBfromFASTA pff = null;
 
 public Substitutor(Structure struc){
-	fitStruc = struc;
+	modifyStructure = struc;
 }
 
-public Structure fakeSubstitute(Structure outStruc, int startAt){
-	//System.out.println(fitStruc.getChain(0).getAtomLength() + " " + outStruc.getChain(0).getAtomLength());
-	for(int i = 0; i < outStruc.getChain(0).getAtomLength() -1; i ++){
-		Chain chain = fitStruc.getChain(0);
+public Structure fakeSubstitute(Structure fitStruc, int getFrom, int getTo, int setFrom){
+	Chain chainFrom = fitStruc.getChain(0);
+	Chain chainTo = modifyStructure.getChain(0);
+	double angle;
+	for(int i = 0; i < getTo - getFrom-1; i ++){
 		try{
-			double angle = Trans.getPhiFix((AminoAcid)chain.getAtomGroup(i + startAt), (AminoAcid)chain.getAtomGroup(i + startAt + 1));
-			Trans.setPhi(outStruc.getChain(0), i, angle);
+			angle = Trans.getPhiFix((AminoAcid)chainFrom.getAtomGroup(i+getFrom), (AminoAcid)chainFrom.getAtomGroup(i+getFrom+1));
+			Trans.setPhi(chainTo, i+setFrom, angle);
+			angle = Trans.getPsiFix((AminoAcid)chainFrom.getAtomGroup(i+getFrom), (AminoAcid)chainFrom.getAtomGroup(i+getFrom+1));
+			Trans.setPsi(chainTo, i+setFrom, angle);
 		}catch(Exception e){
   		e.printStackTrace();
 		}
 	}
+	return modifyStructure;
+}
+
+public Structure fakeSubstitute(Structure outStruc, int startAt){
+	//System.out.println(fitStruc.getChain(0).getAtomLength() + " " + outStruc.getChain(0).getAtomLength());
+	//for(int i = 0; i < outStruc.getChain(0).getAtomLength() -1; i ++){
+	//	Chain chain = fitStruc.getChain(0);
+	//	try{
+	//		double angle = Trans.getPhiFix((AminoAcid)chain.getAtomGroup(i + startAt), (AminoAcid)chain.getAtomGroup(i + startAt + 1));
+	//		Trans.setPhi(outStruc.getChain(0), i, angle);
+	//	}catch(Exception e){
+  //		e.printStackTrace();
+	//	}
+	//}
 	return outStruc;
 }
 
@@ -36,7 +53,7 @@ public Structure fakeSubstitute(String fileName, String identifier, int startAt)
 	pff = new PDBfromFASTA();
 	try{
 		Structure outStruc = pff.makeProtein(pff.pdbFromFile(fileName, identifier));
-	return fakeSubstitute(outStruc, startAt);
+		return fakeSubstitute(outStruc, startAt);
 	}catch(Exception e){
   	e.printStackTrace();
 		return null;
