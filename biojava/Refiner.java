cd6b34d6179fb0a6e2d92c2d5be75b6e7b538eab
PurinLord <purin@tutanota.de>
2015-12-06 13:36:06 -0600

    add Angle decrece in Refiner

--- biojava/Refiner.java
+++ biojava/Refiner.java
@@ -8,6 +8,11 @@ public class Refiner{
 	static int searchStepsCicle;
 	static double anguloInicial;
 	static double anguloFinal;
+	static double iniFin;
+	static double finalFin;
+	static double iniPend;
+	static double finalPend;
+	static int numBusquedas;
 	static String dirStartStruc;
 	static String dirTargetStruc;
 	static String fileDir;
@@ -15,10 +20,15 @@ public class Refiner{
 	static double beast;
 
 	public Refiner(String dirStartStruc, String dirTargetStruc, String fileDir){
-		searchStepsTotal = 3000;
+		searchStepsTotal = 20000;
 		searchStepsCicle = 1;
 		anguloInicial = 0.01;
 		anguloFinal = 0.001;
+		iniFin = 0.0001;
+		finalFin = 0.000001;
+		numBusquedas = 40;
+		iniPend = (anguloInicial-iniFin)/numBusquedas;
+		finalPend = (anguloFinal-finalFin)/numBusquedas;
 		this.dirStartStruc = dirStartStruc;
 		this.dirTargetStruc = dirTargetStruc;
 		this.fileDir = fileDir;
@@ -35,9 +45,9 @@ public class Refiner{
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-		System.out.println(dirStartStruc);
 		SimulatedAnnealing3DProtFromRCC simA = new SimulatedAnnealing3DProtFromRCC(searchStepsTotal,searchStepsCicle,anguloInicial,
 																									anguloFinal,dirStartStruc,dirTargetStruc,fileDir,initSeed);
+		simA.setEnergyType(1);
 		simA.setTemp(4);
 		simA.initialize(1);
 		beast = simA.run(0);
@@ -46,10 +56,11 @@ public class Refiner{
 
 	public static void main(String[] args){
 		Refiner ref = new Refiner(args[0], args[1], args[2]);
-		for(int i=1;i<40;i++){
+		for(int i=ref.numBusquedas;i>0;i--){
 			ref.dirStartStruc = ref.fileDir + "sol_" + ref.simulA() + ".pdb";
 			System.out.println(ref.beast);
-		  //String a = "sol_" + ref.beast + ".pdb";
+			ref.anguloInicial =  ref.iniFin+(i*ref.iniPend);
+			ref.anguloFinal = ref.finalFin+(i*ref.finalPend);
 		}
 	}
 }
