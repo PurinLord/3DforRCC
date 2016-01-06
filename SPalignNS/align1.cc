a0b46d83c772fd8120d2641ebf377bb47878818a
victor <purin@tutanota.de>
2016-01-05 21:46:23 -0600

    add penalty sistem to unpaird atoms

--- SPalignNS/align1.cc
+++ SPalignNS/align1.cc
@@ -398,16 +398,23 @@ void Salign::calscores_all(){
 	double r2 = 0;
 	int i1, i2 = 0;
 		//printf("&%d %d\n",ialign[0].size(),ialign[1].size());
-	int length = min(nA, nB);
-	for(int i=0; i<length; i++){
+	int minLength = min(nA, nB);
+	int maxLength = max(nA, nB);
+	int noMatchCount = 0;
+	for(int i=0; i<minLength; i++){
 		i1 = ialign[0][i];
 		i2 = ialign[1][i];
 			//printf("i- %d %d\n",i1,i2);
-		//if(i1 < 0 || i2 < 0) continue;
+			//printf("xbp- %.2f %.2f %.2f\n",(*xbp)[i2][0],(*xbp)[i2][1],(*xbp)[i2][2]);
+			//printf("xn1- %.2f %.2f %.2f\n",(xn1)[i1][0],(xn1)[i1][1],(xn1)[i1][2]);
+		if(i1 < 0 || i2 < 0){
+			noMatchCount++;
+			continue;
+		}
 		r2 = xn1[i1].distance2((*xbp)[i2]);
+			//printf("r2>%.2f\n",r2);
 		for(int m=0; m<3; m++) TMs[m] += 1. / (1. + r2/DTM2[m]);
 		LG0 += 1. / (1. + r2/D2);
-			//printf("r2>%.2f\n",r2);
 		for(int m=0; m<4; m++){
 				//printf("<%.2f\n",GDT_r2[m]);
 			if(r2 > GDT_r2[m]) continue;
@@ -419,6 +426,13 @@ void Salign::calscores_all(){
 		if(pa->resid[i1] == pb->resid[i2]) nid1 ++;
 			//printf("/ %d %.2f %.2f\n",i ,rms, nali);
 	}
+	int diffLength = maxLength-minLength;
+	if(noMatchCount > diffLength){
+		int penalty = noMatchCount-diffLength;
+		rms += 100 * penalty;
+		nali += penalty;
+	}
+			printf("/ %.2f %.2f\n",rms, nali);
 	scores_all[ieLA] = nali;
 	nali = max(1., nali);
 	rms = sqrt(rms / nali);
