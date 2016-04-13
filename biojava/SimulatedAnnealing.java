20ba75ea3166ca19b81456f54a8cfcb31a29c8bb
victor <purin@tutanota.de>
2016-04-13 12:21:40 -0500

    Split Simul... in 2 clean classes, firs part for supoer multi target

---
+++ biojava/SimulatedAnnealing.java
@@ -0,0 +1,249 @@
+package rccto3d.optimisation;
+
+import java.util.Vector;
+import java.util.Random;
+import java.lang.Math;
+import java.io.*;
+
+import rccto3d.*;
+
+public class SimulatedAnnealing {
+	// Set initial temp
+	private double temp;
+	private double tempPercent = 0.15;
+	private int searchStepsTotal;
+	private int searchStepsCicle;
+
+	// Cooling rate
+	private double coolingRate;
+
+	private boolean rawAccept = false;
+
+	private Random rdm;
+
+	private StructureMannager sm;
+
+	public SimulatedAnnealing(int searchStepsTotal, int searchStepsCicle, StructureMannager sm){
+		this.searchStepsTotal = searchStepsTotal;
+		this.searchStepsCicle = searchStepsCicle;
+		this.temp = 0;
+		this.coolingRate = 0;
+
+		this.sm = sm;
+		rdm =  new Random(System.currentTimeMillis());
+	}
+
+	public void setRawAccept(boolean rawAccept){
+		this.rawAccept = rawAccept;
+	}
+
+	public double getTemp(){
+		return this.temp;
+	}
+
+	public void setTemp(double temp){
+		this.temp = temp;
+	}
+
+	public void setTempPercent(double tempPercent){
+		this.tempPercent = tempPercent;
+	}
+
+	public void setCoolRate(double coolingRate){
+		this.coolingRate = coolingRate;
+	}
+
+	//Calculate the acceptance probability
+	public double acceptanceProbability(double energy, double newEnergy, double temperature) {
+		if(rawAccept){
+			return rawAcceptanceProbability(energy, newEnergy);
+   	}
+		// If the new solution is better, accept it
+		if (newEnergy < energy) return 1.0;
+		// If the new solution is worse, calculate an acceptance probability
+		return Math.exp((energy - newEnergy) / temperature);
+	}
+
+	public double rawAcceptanceProbability(double energy, double newEnergy) {
+		if (newEnergy < energy) return 1.0;
+		return 0.0;
+	}
+
+	public double stdCooling(double temp){
+		return temp * (1-coolingRate);
+	}
+
+	public double linearCooling(double temp){
+		return temp - coolingRate;
+	}
+
+	public double slowCooling(double temp){
+		return temp/Math.log(temp);
+	}
+
+	//verbos 0 - none
+	//			 1 - just solutions
+	//			 2 - all
+	//			 3 - temp calc
+	
+	public double calcInitialTemp(int verbos){
+
+		int count = 0;
+		int searchStepsCicle = 100;
+		double sum = 0;
+		double currentEnergy;
+		while(count < searchStepsCicle){
+			currentEnergy = sm.tempCalc();
+
+			sum += currentEnergy;
+			if(verbos > 2){
+				System.out.println("energy " + currentEnergy + " sum " + sum);
+			}
+			count++;
+		}
+		double prom = sum/count;
+		if(verbos > 1){
+			System.out.println(" promE " + prom);
+		}
+		//double temp = ((3/2)*prom)+(max/2);
+		double temp = prom;//2*max - prom;
+		this.temp = temp * tempPercent;
+		return temp;
+	}
+
+	public void calcCoolRateStd(int verbos, int searchStepsTotal, int searchStepsCicle){
+		double steps = searchStepsTotal/(double)searchStepsCicle;
+		this.coolingRate = 1-(Math.pow((1/this.temp),(1.0/steps)));
+	}
+	
+	public String initialize(int verbos){
+		String outString = null;
+		if(temp == 0){
+			calcInitialTemp(verbos);
+		}
+		if(coolingRate == 0){
+			calcCoolRateStd(verbos, searchStepsTotal, searchStepsCicle);
+		}
+
+		if (verbos > 0){
+			outString = "temp "+temp +" coolRate "+coolingRate+" totalSearchSteps "+searchStepsTotal+" searchStepsCicle "+searchStepsCicle;
+			if (verbos > 1){
+				System.out.println(outString);
+				System.out.print("\n");
+			}
+			return outString;
+		}
+		return null;
+	}
+
+	public double run(int verbos){
+		//Mesure time
+		long elapsedTime = 0;
+		if (verbos > 0){
+			elapsedTime = System.nanoTime();
+		}
+		
+		double currentEnergy[] = {0.0,0.0,0.0};
+		double neighbourEnergy[] = {0.0,0.0,0.0};
+		double distance_ini[] = {0.0,0.0,0.0};
+		double best;
+
+		// Initialize intial solution
+		distance_ini[0] = sm.calcEnergy();
+		
+		if (verbos > 0){
+			System.out.println("Initial solution distance: "+distance_ini[0]+" "+distance_ini[1]+" "+distance_ini[2]);
+		}
+		
+		// Set as current best
+		best = sm.calcEnergy();
+		    
+		// Create new neighbour 3d model
+		 
+		currentEnergy = distance_ini;
+
+		// Loop until system has cooled
+		for (;temp > 1;temp = stdCooling(temp)){
+			// Search steps
+			sm.angleCooling();
+			sm.addAngleStep(searchStepsCicle, searchStepsTotal);
+
+			for(int step = 0; step < searchStepsCicle; step++){
+				// Get a random conformation for this new neighbor
+				sm.alterConformation();
+				// Get energy of solution
+				neighbourEnergy[0] = sm.calcEnergy();
+			
+				// Decide if we should accept the neighbour
+				if (acceptanceProbability(currentEnergy[0], neighbourEnergy[0], temp) > rdm.nextDouble()) {
+					sm.acceptModel();
+					//PDBfromFASTA.writePDB(fileDir + "struc_fit.pdb", struc_fit);
+					currentEnergy = neighbourEnergy;
+					if (verbos > 0){
+						if (verbos > 1){
+							long time = System.nanoTime() - elapsedTime;
+							System.out.println(temp + "> " + currentEnergy[0] + " time: " + time/3600000000000.0);
+						}else{
+							System.out.println(temp + "> " + currentEnergy[0]);
+						}
+					}
+				}
+			
+				// Keep track of the best solution found
+				if (neighbourEnergy[0] < best){
+					best = neighbourEnergy[0];
+					sm.setModelAsBeast();
+					//PDBfromFASTA.writePDB(fileDir + "sol_" + best + ".pdb", struc_fit);
+					if(best == 0){
+						temp = 0;
+					}
+				}
+				if (verbos > 1){
+					sm.printEnergy();
+				}
+			}
+		}
+		
+		if (verbos > 0){
+			System.out.println("Final solution distance: " + best);//.getDistance());
+			elapsedTime = System.nanoTime() - elapsedTime;
+			System.out.println("Total execution time: " + elapsedTime/3600000000000.0);
+		}
+		sm.saveBest();
+		return best;
+	}
+
+	public static void main(String[] args){
+		int searchStepsTotal = 3000;
+		int searchStepsCicle = 1;
+	
+		double anguloInicial = 0.05;
+		double anguloFinal = 0.001;
+	
+		String dirStartStruc = args[0];
+		String dirTargetStruc = args[1];
+		String fileDir = "out/";
+		long initSeed = (long)(1000*Math.random());
+		if(args.length>2){
+			fileDir = args[2];
+		}
+		SimulatedAnnealing3DProtFromRCC simA = new SimulatedAnnealing3DProtFromRCC(searchStepsTotal,searchStepsCicle,anguloInicial,
+																									anguloFinal,dirStartStruc,dirTargetStruc,fileDir,initSeed);
+		//SimulatedAnnealing3DProtFromRCC simA = new SimulatedAnnealing3DProtFromRCC(args[0], args[1]);
+		simA.setTemp(2.5);
+		//Substitutor sub = new Substitutor(Trans.readPDB(args[1]));
+		//Vector<Vector<Integer>> divition = new Vector<Vector<Integer>>(5);
+		//Vector<Integer> segment = new Vector<Integer>(2);
+		//segment.add(4); segment.add(16); divition.add(segment); segment = new Vector<Integer>(2);
+		//segment.add(26); segment.add(5); divition.add(segment); segment = new Vector<Integer>(2);
+		//segment.add(33); segment.add(25); divition.add(segment); segment = new Vector<Integer>(2);
+		//segment.add(67); segment.add(35); divition.add(segment); segment = new Vector<Integer>(2);
+		//segment.add(110); segment.add(31); divition.add(segment);
+		//sub.setDivition(divition);
+		//simA.setSubsitutor(sub);
+		//sub.createDivition(4, 20, 20, 4, 2);
+		//simA.setSubsitutor(PDBfromFASTA.readPDB(args[3]));
+		simA.initialize(2);
+		simA.run(2);
+	}
+}
---
+++ biojava/StructureMannager.java
@@ -0,0 +1,598 @@
+package rccto3d.optimisation;
+
+import java.util.Vector;
+import java.util.Random;
+import java.lang.Math;
+import java.io.*;
+
+import org.biojava.bio.structure.Structure;
+import org.biojava.bio.structure.Chain;
+import org.biojava.bio.structure.AminoAcid;
+
+import org.biojava.bio.structure.Atom;
+import org.biojava.bio.structure.StructureTools;
+import org.biojava.bio.structure.align.*;
+import org.biojava.bio.structure.align.model.AFPChain;
+import org.biojava.bio.structure.align.fatcat.calc.FatCatParameters ;
+import org.biojava.bio.structure.align.fatcat.FatCatRigid;
+import org.biojava.bio.structure.align.ce.CeParameters;
+import org.biojava.bio.structure.align.ce.CeMain;
+import org.biojava.bio.structure.align.seq.SmithWaterman3Daligner;
+
+import rccto3d.*;
+import rccto3d.Trans;
+import rccto3d.PDBfromFASTA;
+
+//import org.SmithWaterman3Daligner;
+
+public class StructureMannager {
+
+	private int initialStructureType;
+	// 0 fasta
+	// 1 pdb
+	// 2 get target
+	private int targetStructureType;
+	// 0 un target
+	// 1 multi targat
+	private int energyFunction;
+	// 0 SmithWaterman
+	// 1 SPalignNS
+	// 2 RCC
+	private int modifyMethod;
+	// 0 min-max change
+	// 1 target mold
+	// 2 all random
+	// 3 substitutor
+
+	//factor de cambio para Phi y Psi
+	private double initPhi;
+	private double initPsi;
+	private double minPhi;
+	private double minPsi;
+	private double cambioPhi;
+	private double cambioPsi;
+	private double angleSteps = 0;
+
+	private String fastaID = "a";
+	private String fileDir = "";
+
+	private Structure struc_ini = null;
+	private Structure[] struc_target;
+	private Structure struc_fit = null;
+	private Structure struc_model = null;
+	private Structure struc_best = null;
+
+	private long initSeed;
+
+	private int multiEnergy = 1;
+
+	private Substitutor sub = null;
+
+	//TODO no se usa para el Calculo de energía
+	private int targetRCC[][]; 
+
+	private Structure target = null;
+	
+	public StructureMannager(int initialStructureType, int targetStructureType, int energyFunction, int modifyMethod){
+		this.initialStructureType = initialStructureType;
+		this.targetStructureType = targetStructureType;
+		this.modifyMethod = modifyMethod;
+		this.energyFunction = energyFunction;
+	}
+
+	public void setConditions(double anguloInicial, double anguloFinal, String[] fileName, String fileDir, long initSeed){
+
+		this.initPhi = this.cambioPhi = this.initPsi = this.cambioPsi = anguloInicial;
+		this.minPhi = this.minPsi = anguloFinal;
+
+		this.fileDir = fileDir;
+		this.initSeed = initSeed;
+	}
+
+	public void setFastaID(String id){
+		fastaID = id;
+	}
+
+	public void setSubsitutor(Substitutor sub){
+		this.sub = sub;
+	}
+
+	public int[] calcRCC(String s1){
+		return calcRCC(s1, fileDir);
+	}
+
+
+	public String loadStructures(String[] fileName, int verbos){
+		// Load Protein Sequence
+		PDBfromFASTA pff = null;
+		String pdb = null;
+		String outString = null;
+		
+		//load initial
+		switch (initialStructureType){
+			case 0: //fasta
+				try{
+					pff = new PDBfromFASTA();
+					pdb = pff.pdbFromFile(fileName[0], fastaID);
+				
+					// Build initial Protein 3D structure
+					struc_ini = pff.randomShapeProtein(pdb, initSeed);
+					
+				}catch(Exception e){
+					e.printStackTrace();
+				}
+				break;
+
+			case 1: //pdb
+				struc_ini = PDBfromFASTA.readPDB(fileName[0]);
+				break;
+
+			case 2: //no init Copy target
+				if(targetStructureType == 0){
+					struc_ini = PDBfromFASTA.readPDB(fileName[0]);
+					struc_ini = alterConformationAll(struc_ini, 180, 180);
+					target = PDBfromFASTA.readPDB(fileName[0]);
+				}
+				// TODO elegir un objetibo al azar
+				break;
+		}
+
+		if(modifyMethod == 3){
+			if(sub != null){
+				if(initialStructureType != 1){
+					struc_ini = sub.fakeSubstitute(struc_ini);
+				}
+			}else{
+				sub = new Substitutor(struc_ini);
+				sub.randomInitialize();
+			}
+		}
+		struc_best = (Structure)struc_ini.clone();
+		PDBfromFASTA.writePDB(fileDir + "struc_ini.pdb", struc_ini);
+
+		//Load target
+		switch (targetStructureType){
+			case 0:
+				struc_target = new Structure[1];
+				if(initialStructureType != 2){
+					struc_target[0] = PDBfromFASTA.readPDB(fileName[1]);
+				}else{
+					struc_target[0] = PDBfromFASTA.readPDB(fileName[0]);
+				}
+				break;
+			case 1:
+				int i = 0;
+				if(initialStructureType != 2){
+					i++;
+				}
+				struc_target = new Structure[fileName.length - i];
+				for(String struc : fileName){
+				for(;i<fileName.length;i++){
+					struc_target[i] = PDBfromFASTA.readPDB(fileName[i]);
+				}
+				break;
+		}
+
+		if(targetStructureType == 0){
+			PDBfromFASTA.writePDB(fileDir + "struc_target.pdb", struc_target[0]);
+		}
+		if(targetStructureType == 1){
+			int i = 0;
+			for(Structure struc : struc_target){
+				PDBfromFASTA.writePDB(fileDir + "struc_target_"+i+".pdb", struc_target[i]);
+				i ++;
+			}
+		}
+
+		if(energyFunction == 2 || multiEnergy > 1){
+			int i = 0;
+			for(Structure struc : struc_target){
+				targetRCC[i] = calcRCC(struc);
+				i++;
+			}
+		}
+
+		if (verbos > 0){
+			outString = fileName[0] + " " + fileName[1]; //TODO un for para poner todo filename
+			outString += "\nangulo Inicial " + initPhi + " angulo final " + minPhi + " initSeed " + initSeed;
+			if(modifyMethod == 3){
+				Vector<Vector<Integer>> divition = sub.getDivition();
+				outString += "\n" + divition;
+			}
+			if (verbos > 1){
+				if(energyFunction == 2 || multiEnergy > 1){
+					for(int[] vect : targetRCC){
+						for(int i : vect){System.out.print(i + " ");}
+					}
+				}
+				System.out.println(outString);
+				System.out.print("\n");
+			}
+			return outString;
+		}
+		return null;
+	}
+
+	public static int[] calcRCC(String s1, String tmpdir){
+		int rcc[] = new int[26];
+		try{
+		String s2 = "A";
+		Process p = Runtime.getRuntime().exec("python create_26dvRCC.py "+s1+" "+s2 +" "+tmpdir);
+		BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		String ret = in.readLine();
+		String val = "";
+		int j = 0;
+		for(int i = 0; i < ret.length(); i++){
+			char c = ret.charAt(i);
+			if(c==','){
+				rcc[j] = Integer.valueOf(val);
+				val = "";
+				j++;
+			}else{
+				val+=c;
+			}
+		}
+		rcc[j] = Integer.valueOf(val);
+		}catch(Exception e){
+  		e.printStackTrace();
+		}
+		return rcc;
+	}
+
+	//TODO
+	public int[] calcRCC(Structure struc) {
+		int[] result = new int[26];
+		int i=0;
+		
+		for(i=0; i<26; i++) result[i]=0;
+		
+		return result;
+	} // end calcRCC
+
+	public double calcRMSD(Structure struc_target, Structure struc_current){
+		double currentRMSd = 0.0;
+		try {
+			StructureAlignment algorithm  = StructureAlignmentFactory.getAlgorithm(FatCatRigid.algorithmName);
+
+			Atom[] ca1 = StructureTools.getAtomCAArray(struc_target);
+			Atom[] ca2 = StructureTools.getAtomCAArray(struc_current);
+			
+			FatCatParameters params = new FatCatParameters();
+			
+			AFPChain afpChain = algorithm.align(ca1,ca2,params);            
+			
+			currentRMSd = afpChain.getChainRmsd();
+		} catch (Exception e) {
+		e.printStackTrace();
+		}
+		return currentRMSd;
+	}
+
+	public double calcBackboneRMSD(Structure struc_target, Structure struc_current){
+		double currentRMSd = 0.0;
+		try {
+			StructureAlignment algorithm  = StructureAlignmentFactory.getAlgorithm(FatCatRigid.algorithmName);
+
+			Atom[] ca1 = StructureTools.getBackboneAtomArray(struc_target);
+			Atom[] ca2 = StructureTools.getBackboneAtomArray(struc_current);
+			
+			FatCatParameters params = new FatCatParameters();
+			
+			AFPChain afpChain = algorithm.align(ca1,ca2,params);            
+			
+			currentRMSd = afpChain.getChainRmsd();
+		} catch (Exception e) {
+		e.printStackTrace();
+		}
+		return currentRMSd;
+	}
+
+	public double calcRMSD2(Structure struc_target, Structure struc_current){
+		double currentRMSd = 0.0;
+		try {
+ 			StructureAlignment algorithm  = StructureAlignmentFactory.getAlgorithm(CeMain.algorithmName);
+ 			
+			Atom[] ca1 = StructureTools.getAtomCAArray(struc_target);
+			Atom[] ca2 = StructureTools.getAtomCAArray(struc_current);
+ 			
+ 			// get default parameters
+ 			CeParameters params = new CeParameters();
+ 			// set the maximum gap size to unlimited 
+ 			//params.setMaxGapSize(-1);
+ 			
+ 			AFPChain afpChain = algorithm.align(ca1,ca2,params);            
+
+			currentRMSd = afpChain.getChainRmsd();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return currentRMSd;
+	}
+
+	public double calcBackboneRMSD2(Structure struc_target, Structure struc_current){
+		double currentRMSd = 0.0;
+		try {
+ 			StructureAlignment algorithm  = StructureAlignmentFactory.getAlgorithm(CeMain.algorithmName);
+ 			
+			Atom[] ca1 = StructureTools.getBackboneAtomArray(struc_target);
+			Atom[] ca2 = StructureTools.getBackboneAtomArray(struc_current);
+ 			
+ 			// get default parameters
+ 			CeParameters params = new CeParameters();
+ 			// set the maximum gap size to unlimited 
+ 			params.setMaxGapSize(-1);
+ 			
+ 			AFPChain afpChain = algorithm.align(ca1,ca2,params);            
+
+			currentRMSd = afpChain.getChainRmsd();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return currentRMSd;
+	}
+
+	public double calcRMSD3(Structure struc_current, Structure struc_target){
+		double currentRMSd = 0.0;
+		try {
+			Atom[] ca1 = StructureTools.getAtomCAArray(struc_current);
+			Atom[] ca2 = StructureTools.getAtomCAArray(struc_target);
+			SmithWaterman3Daligner swAligner = new SmithWaterman3Daligner();
+ 			
+ 			AFPChain afpChain = swAligner.align(ca1,ca2);            
+
+			currentRMSd = afpChain.getChainRmsd();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return currentRMSd*100;
+	}
+
+	public double calcRMSD_New(String dir1, String dir2){
+		double rmsd=0;
+		try{
+		String s1 = "-rmsOnly -pair";
+		Process p = Runtime.getRuntime().exec("./SPalignNS "+s1+" "+dir1+" "+dir2);
+		BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		rmsd = Double.parseDouble(in.readLine());
+		}catch(Exception e){
+  		e.printStackTrace();
+		}
+		return rmsd;
+	}
+
+	public double calcRMSD_New(Structure struc_current, Structure struc_target){
+		PDBfromFASTA.writePDB(fileDir + "struc_1.pdb", struc_current);
+		PDBfromFASTA.writePDB(fileDir + "struc_2.pdb", struc_target);
+		return calcRMSD_New(fileDir + "struc_1.pdb", fileDir + "struc_2.pdb");
+	}
+
+ 	static {
+  	System.loadLibrary("SPalig");
+	}
+	public native double calcRMSD_New_fast(String pdb_struc_current, String pdb_struc_target);
+	public double calcRMSD_New_fast(Structure struc_current, Structure struc_target){
+		//if(target_pdb == null){
+		//	target_pdb = struc_target.toPDB();
+		//}
+		String s1 = struc_target.toPDB();
+		String s2 = struc_current.toPDB();
+		return 100*calcRMSD_New_fast(s1, s2);
+	}
+	
+	//public double calcRMSD_New_fast(Structure struc_current, Structure struc_target){
+	//	SPalignNS spalns = new SPalignNS();
+	//	return spalns.calcRMSD(struc_current, struc_target);
+	//}
+
+	public double calcRCCSimilarity(int[] rcc1, int[] rcc2){
+		int sum = 0;
+		int i=0;
+		for(i=0; i<26; i++){
+			sum += Math.pow(rcc1[i] - rcc2[i], 2.0);
+		}
+		return Math.sqrt(sum);
+	}
+
+	public double calcEnergy(Structure[] s1, Structure s2){
+		double[] energy = new double[target.length];
+		int i = 0;
+		for(Structure struc : target){
+			switch(energyFunction){
+				case 0:
+					energy[i] = calcRMSD3(struc, fit);
+					break;
+
+				case 1:
+					energy[i] = calcRMSD_New_fast(struc, fit);
+					break;
+
+				case 2:
+					PDBfromFASTA.writePDB(fileDir + "struc_1.pdb", struc);
+					int RCC1[] = calcRCC(fileDir + "struc_1.pdb");
+					PDBfromFASTA.writePDB(fileDir + "struc_2.pdb", fit);
+					int RCC2[] = calcRCC(fileDir + "struc_2.pdb");
+					energy[i] = calcRCCSimilarity(RCC1, RCC2);;
+					break;
+			}
+			i++;
+		}
+		if(targetStructureType != 1){
+			return energy[0];
+		}else{
+			//TODO multi target
+			energy = 100000000;
+		}
+	}
+
+	public double multiCalcEnergy(double[] energy){
+		return 0;
+	}
+
+	public double calcEnergy(){
+		return calcEnergy(struc_target, struc_fit);
+	}
+
+	public void alterConformation(){
+		switch(modifyMethod){
+			case 0:
+				//alterConformationSome(minCambio,maxCambio, struc_fit);
+				break;
+
+			case 1:
+				struc_model = alterConformationAll(struc_fit, target);
+				break;
+
+			case 2:
+				struc_model = alterConformationAll(struc_fit);
+				break;
+
+			case 3:
+				struc_model = alterConformationParts(struc_fit);
+				break;
+		}
+	}
+
+	public void acceptModel(){
+		struc_fit = (Structure)struc_model.clone();
+	}
+
+	public void setModelAsBeast(){
+		struc_best = (Structure)struc_model.clone();
+	}
+
+	public void saveBest(){
+		double best = calcEnergy(struc_target, struc_best);
+		PDBfromFASTA.writePDB(fileDir + "sol_" + best + ".pdb", struc_best);
+	}
+
+	public void printEnergy(){
+		//if(energyFunction == 2){
+		//	for(int i : modelRCC){System.out.print(i + " ");}
+		//	System.out.print("\n");
+		//}
+		if(multiEnergy > 1){
+			double energy = calcEnergy(struc_target, struc_model);
+			System.out.println(energy);
+		}
+	}
+
+	public Structure alterConformationSome(int minCambio, int maxCambio, Structure struc){
+		Random rdm =  new Random(System.currentTimeMillis());
+		Structure alterStruc = (Structure)struc.clone();
+		Chain chain = alterStruc.getChain(0);
+		int largo = chain.getAtomLength();
+		int to = minCambio + (int)(rdm.nextDouble()*(maxCambio - minCambio));
+		for(int i=0; i<to; i++){
+			int dAmino = (int) (largo * rdm.nextDouble());
+			double dPsi = Math.round(cambioPsi * 2*(rdm.nextDouble() - 0.5));
+			double dPhi = Math.round(cambioPhi * 2*(rdm.nextDouble() - 0.5));
+			Trans.rotatePsi(chain, dAmino, dPsi);
+			Trans.rotatePhi(chain, dAmino, dPhi);
+		}
+		return alterStruc;
+	}
+
+	//TODO ROUND
+	public Structure alterConformationAll(Structure struc){
+		Random rdm =  new Random(System.currentTimeMillis());
+		Structure alterStruc = (Structure)struc.clone();
+		Chain chain = alterStruc.getChain(0);
+		int largo = chain.getAtomLength();
+		for(int i=0; i<largo; i++){
+			double dPsi = cambioPsi * 2*(rdm.nextDouble() - 0.5);
+			double dPhi = cambioPhi * 2*(rdm.nextDouble() - 0.5);
+			//System.out.println(dPhi +" "+ dPsi);
+			Trans.rotatePsi(chain, i, dPsi);
+			Trans.rotatePhi(chain, i, dPhi);
+		}
+		return alterStruc;
+	}
+
+	public Structure alterConformationParts(Structure struc){
+		Random rdm =  new Random(System.currentTimeMillis());
+		Structure alterStruc = (Structure)struc.clone();
+		Chain chain = alterStruc.getChain(0);
+		int largo = chain.getAtomLength();
+		double dPsi;
+		double dPhi;
+		int index = 0;
+		Vector<Integer> segment = sub.getDivition().elementAt(index);
+		for(int i=0; i<largo; i++){
+			if(i == segment.elementAt(0)){
+				i += segment.elementAt(1);
+				index++;
+				if(index < sub.getDivition().size()){
+					segment = sub.getDivition().elementAt(index);
+				}
+			}
+			dPsi = cambioPsi * 2*(rdm.nextDouble() - 0.5);
+			dPhi = cambioPhi * 2*(rdm.nextDouble() - 0.5);
+			//System.out.println(dPhi +" "+ dPsi);
+			Trans.rotatePsi(chain, i, dPsi);
+			Trans.rotatePhi(chain, i, dPhi);
+		}
+		return alterStruc;
+	}
+
+	public Structure alterConformationAll(Structure struc, double deltaPsi, double deltaPhi){
+		Random rdm =  new Random(System.currentTimeMillis());
+		Structure alterStruc = (Structure)struc.clone();
+		Chain chain = alterStruc.getChain(0);
+		int largo = chain.getAtomLength();
+		for(int i=0; i<largo; i++){
+			double dPsi = deltaPsi * 2*(rdm.nextDouble() - 0.5);
+			double dPhi = deltaPhi * 2*(rdm.nextDouble() - 0.5);
+			//System.out.println(i +" "+ dPhi +" "+ dPsi);
+			Trans.rotatePsi(chain, i, dPsi);
+			Trans.rotatePhi(chain, i, dPhi);
+		}
+		return alterStruc;
+	}
+
+	public Structure alterConformationAll(Structure struc, Structure target){
+		Random rdm =  new Random(System.currentTimeMillis());
+		Chain chainT = target.getChain(0);
+		Structure alterStruc = (Structure)struc.clone();
+		Chain chain = alterStruc.getChain(0);
+		int largo = chain.getAtomLength();
+		AminoAcid a1;
+		AminoAcid a2;
+		for(int i=0; i <largo; i++){
+			int dAmino = (int) ((largo -1) * rdm.nextDouble());
+			try{
+			a1 = (AminoAcid)chainT.getAtomGroup(dAmino);
+			a2 = (AminoAcid)chainT.getAtomGroup(dAmino + 1);
+			double dPsi = Trans.getPhi(a1, a2);
+			double dPhi = Trans.getPsi(a1, a2);
+			Trans.rotatePsi(chain, i, dPsi);
+			Trans.rotatePhi(chain, i, dPhi);
+    	}catch (Exception e) {
+				//System.out.println(">" + largo +" "+ i);
+    		e.printStackTrace();
+    	}
+		}
+		return alterStruc;
+	}
+
+	//
+	//Random de 0 error
+	//al dividir salen 0 donde no deberían
+	//todo mayor que 0!!!!!
+
+	public void angleCooling(){
+		cambioPhi = initPhi*Math.exp(-angleSteps/(-1.0/Math.log(minPhi/initPhi)));
+		cambioPsi = cambioPhi;
+	}
+	public void addAngleStep(int searchStepsCicle, int searchStepsTotal){
+		angleSteps += (double)searchStepsCicle/searchStepsTotal;
+	}
+
+	public double tempCalc(){
+		//Structure alterStruc = sm.alterConformationAll(alterStruc, 180, 180);
+		Structure alterStruc = alterConformationAll(struc_ini, 180, 180);
+		return calcEnergy(struc_target,alterStruc);
+	}
+
+	public static void main(String[] args){
+	}
+}
